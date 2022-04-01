1aca40796519ce2afc539ad2bfcaf14f50cb16c8
LinXuan <hou_xiaoxuan@qq.com>
2022-04-01 20:06:28 +0800

    有bug,但是写完

--- CodeCraft-2022/src/CodeCraft-2022.cpp
+++ CodeCraft-2022/src/CodeCraft-2022.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include "config.h"
 #include "data.h"
+#include "hungarian.h"
 int main() {
     std::cout << INPUT <<std::endl;
     Data data = read_file();
     // 实现功能
+    Hungarian solve(data);
+    Distribution distribution = solve.excute();
+    output_distribution(data, distribution);
+
     std::cout<<"over"<<std::endl;
     return 0;
 }
