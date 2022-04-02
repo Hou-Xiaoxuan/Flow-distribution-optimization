21c8e3bc0b78e121fe80f4350e00d0192c077dbb
xv_rong <1187518864@qq.com>
2022-04-02 16:56:43 +0800

    读数据时, 直接将 edge_site 按照 site_bandwidth 从大到小排序

--- CodeCraft-2022/src/data.cpp
+++ CodeCraft-2022/src/data.cpp
@@ -1,16 +1,15 @@
 /*
  * @Author: LinXuan
  * @Date: 2022-03-31 19:24:12
- * @Description: 
- * @LastEditors: LinXuan
- * @LastEditTime: 2022-03-31 21:31:02
+ * @Description:
+ * @LastEditors: xv_rong
+ * @LastEditTime: 2022-04-02 16:55:06
  * @FilePath: /FDO/CodeCraft-2022/src/data.cpp
  */
 #include "data.h"
 
 /*read_file的工具函数，读取一行文件流并按照指定分隔符分隔，返回分割后的vector<string>*/
-vector<string> get_split_line(ifstream &file, char delim)
-{
+vector<string> get_split_line(ifstream &file, char delim) {
     // 读取文件流的一行，使用delim分割
     vector<string> result;
     string line;
@@ -26,44 +25,42 @@ vector<string> get_split_line(ifstream &file, char delim)
 }
 
 /*从文件中获取数据，储存到一个Data结构体中并返回*/
-Data read_file()
-{
+Data read_file() {
     Data data;
     ifstream file;
     vector<string> split_line;
 
     /*读取config.ini*/
     file.open(INPUT + "config.ini");
-    split_line = get_split_line(file, '\n');    // 忽略首行
-    split_line = get_split_line(file, '=');     // qos_constraint
+    split_line = get_split_line(file, '\n'); // 忽略首行
+    split_line = get_split_line(file, '=');  // qos_constraint
     data.qos_constraint = stoi(split_line[1]);
-    split_line = get_split_line(file, '=');     //  base_cost
+    split_line = get_split_line(file, '='); //  base_cost
     data.base_cost = stoi(split_line[1]);
     file.close();
-
 
     /*读取demand.csv*/
     file.open(INPUT + "demand.csv");
     // customer_site及其逆映射
-    split_line = get_split_line(file, ',');     // 首行，mtime+stream_id+...customer_site
+    split_line = get_split_line(file, ','); // 首行，mtime+stream_id+...customer_site
     split_line.erase(split_line.begin());
     split_line.erase(split_line.begin());
     data.customer_site = split_line;
-    for(size_t i=0; i<data.customer_site.size(); i++){
+    for (size_t i = 0; i < data.customer_site.size(); i++) {
         data.re_customer_site[data.customer_site[i]] = i;
     }
-    
+
     // stream_id与demand
     // 没有说时间按顺序给出，留一个 mtime -> index  的临时映射处理
     unordered_map<string, size_t> re_mtime_index;
-    while(file.eof()==false)
-    {
+    while (file.eof() == false) {
         split_line = get_split_line(file, ',');
-        if(split_line.empty()) break;
+        if (split_line.empty())
+            break;
         // 处理m_time的映射
         string smtime = split_line[0];
         size_t m_time;
-        if(re_mtime_index.count(smtime)==0){
+        if (re_mtime_index.count(smtime) == 0) {
             m_time = re_mtime_index.size();
             re_mtime_index[smtime] = m_time;
             data.demand.push_back(vector<vector<pair<int, int>>>(data.customer_site.size()));
@@ -72,33 +69,44 @@ Data read_file()
         string sstream_type = split_line[1];
         int stream_type;
         // 处理stream_type及其映射
-        if(data.re_stream_type.count(sstream_type) == 0){
+        if (data.re_stream_type.count(sstream_type) == 0) {
             stream_type = data.stream_type.size();
             data.stream_type.push_back(sstream_type);
             data.re_stream_type[sstream_type] = stream_type;
-        } else{
+        } else {
             stream_type = data.re_stream_type[sstream_type];
         }
 
-        for(size_t i=2; i<split_line.size(); i++){
-            data.demand[m_time][i-2].push_back(make_pair(stoi(split_line[i]), stream_type));
+        for (size_t i = 2; i < split_line.size(); i++) {
+            data.demand[m_time][i - 2].push_back(make_pair(stoi(split_line[i]), stream_type));
         }
     }
     file.close();
 
-
     /*读取site_bandwith.csv*/
     file.open(INPUT + "site_bandwidth.csv");
     split_line = get_split_line(file, ','); // 忽略掉第一行
-    while (file.eof() == false)
-    {
+    while (file.eof() == false) {
         split_line = get_split_line(file, ',');
-        if(split_line.empty()) break;
+        if (split_line.empty())
+            break;
         data.site_bandwidth.push_back(stoi(split_line[1]));
         data.edge_site.push_back(split_line[0]);
         data.re_edge_site[split_line[0]] = data.edge_site.size() - 1;
     }
     file.close();
+
+    // 对site_bandwidth按照带宽从大到小排序
+    vector<pair<int, string>> tmp(data.site_bandwidth.size());
+    for (size_t i = 0; i < data.site_bandwidth.size(); ++i) {
+        tmp[i] = {data.site_bandwidth[i], data.edge_site[i]};
+    }
+    sort(tmp.begin(), tmp.end(), greater<pair<int, string>>());
+    for (size_t i = 0; i < data.site_bandwidth.size(); ++i) {
+        data.site_bandwidth[i] = tmp[i].first;
+        data.edge_site[i] = tmp[i].second;
+        data.re_edge_site[tmp[i].second] = i;
+    }
 
     /*读取qos.csv*/
     file.open(INPUT + "qos.csv");
@@ -112,10 +120,10 @@ Data read_file()
 
     data.qos = vector<vector<int>>(data.customer_site.size(), vector<int>(data.edge_site.size(), 0));
 
-    while (file.eof() == false)
-    {
+    while (file.eof() == false) {
         split_line = get_split_line(file, ',');
-        if(split_line.empty()) break;
+        if (split_line.empty())
+            break;
         int edge_index = data.re_edge_site[split_line[0]];
         for (size_t i = 1; i < split_line.size(); ++i) {
             data.qos[tmp_customer_list[i - 1]][edge_index] = stoi(split_line[i]);
@@ -128,24 +136,19 @@ Data read_file()
 }
 
 /*按照规定格式输出一个分配好的distribution*/
-void output_distribution(const Data &data, const Distribution &distribution)
-{
+void output_distribution(const Data &data, const Distribution &distribution) {
     ofstream fout(OUTPUT + "solution.txt");
     // 遍历时刻
-    for (const auto &distribution_t : distribution)
-    {
+    for (const auto &distribution_t : distribution) {
         // 遍历customer_site
-        for (size_t i = 0; i < distribution_t.size(); i++)
-        {
+        for (size_t i = 0; i < distribution_t.size(); i++) {
             fout << data.customer_site[i] << ":";
-            for (size_t j = 0; j < distribution_t[i].size(); j++)
-            {
-                int edge_site = distribution_t[i][j].first;
+            for (size_t j = 0; j < distribution_t[i].size(); j++) {
+                // int edge_site = distribution_t[i][j].first;
                 int stream_type = distribution_t[i][j].second;
                 if (j == 0) {
                     fout << "<" + data.edge_site[i] + "," + data.stream_type[stream_type] + ">";
-                }
-                else {
+                } else {
                     fout << ",<" + data.edge_site[i] + "," + data.stream_type[stream_type] + ">";
                 }
             }
