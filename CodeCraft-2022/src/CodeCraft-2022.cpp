1412df253027ed1eed661e9a675f16592558bb88
LinXuan <hou_xiaoxuan@qq.com>
2022-04-02 22:23:23 +0800

    delete 错误提交

--- CodeCraft-2022/src/CodeCraft-2022.cpp
+++ CodeCraft-2022/src/CodeCraft-2022.cpp
@@ -1,21 +1,17 @@
 /*
  * @Author: xv_rong
  * @Date: 2022-03-31 20:44:44
- * @LastEditors: xv_rong
+ * @LastEditors: LinXuan
  */
 #include "config.h"
 #include "data.h"
-#include "ffd.cpp"
 #include <iostream>
 int main() {
     std::cout << INPUT << std::endl;
     Data data = read_file();
 
     // 实现功能
-    FFD ffd(data);
-    Distribution distribution = ffd.excute();
 
-    output_distribution(data, distribution);
     std::cout << "over" << std::endl;
     return 0;
 }
